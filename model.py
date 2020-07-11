15fc063f6c9bf7bc386c7ae46236dc9218f5302b
Jeffrey Yun <thisishaoyu@gmail.com>
2020-07-10 21:13:35 -0700

    Compatibility with Udacity workspace

--- model.py
+++ model.py
@@ -4,15 +4,14 @@ import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
 import keras
-from keras.model import Sequential
-from keras.layers import Flatten, Dense, Lambda
-from keras.layers.convolutional import Convolution2D, Cropping2D, Dropout
-from keras.layers.pooling import MaxPooling2D
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, MaxPooling2D
+from keras.layers.convolutional import Conv2D
 
 
 """ Import data and train """
 lines = []
-with open("../data/driving_log.csv", 'r') as f:
+with open("./data/driving_log.csv", 'r') as f:
     reader = csv.reader(f)
     for line in reader:
         lines.append(line)
@@ -33,7 +32,7 @@ def generator(samples, batch_size=32):
             for line in batch_samples:
                 for i in range(3):
                     fname = line[0].split('/')[-1] # source_path.split()[-1]
-                    local_path = f"../data/IMG/{fname}"
+                    local_path = "../data/IMG/" + fname
                     image = cv2.imread(local_path)
                     measurement = float(line[3]) + steer_corr_factor[i]
                     # Add original image and measurement
@@ -59,15 +58,15 @@ valid_gen = generator(valid_samples, batch_size=BATCH_SIZE)
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))   # Normalizing
 model.add(Cropping2D(cropping=((70,25), (0,0))))    # Cropping (out the top noisy scenery)
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
+model.add(Conv2D(24,5,5,subsample=(2,2),activation='relu'))
 model.add(Dropout(0.1))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+model.add(Conv2D(36,5,5,subsample=(2,2),activation='relu'))
 model.add(Dropout(0.15))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+model.add(Conv2D(48,5,5,subsample=(2,2),activation='relu'))
 model.add(Dropout(0.2))
-model.add(Convolution2D(64,5,5,activation='relu'))
+model.add(Conv2D(64,5,5,activation='relu'))
 model.add(Dropout(0.25))
-model.add(Convolution2D(64,5,5,activation='relu'))
+model.add(Conv2D(64,5,5,activation='relu'))
 model.add(Dropout(0.3))
 model.add(Flatten())
 model.add(Dense(100))
